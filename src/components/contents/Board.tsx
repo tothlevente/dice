562bbbce15f506a63f8c740227b254cb78bf90e3
Levente <168279599+tothlevente@users.noreply.github.com>
2024-12-23 07:59:13 +0000

    Develop Board.tsx

--- src/components/contents/Board.tsx
+++ src/components/contents/Board.tsx
@@ -1,3 +1,25 @@
-export default function Board() {
-  return <div className="board"></div>;
+import Dice from "../dice";
+
+export default function Board({
+  side,
+  shaking,
+  color,
+  handleRoll,
+}: {
+  side: number;
+  shaking: boolean;
+  color: string;
+  handleRoll: () => void;
+}) {
+  return (
+    <div className="board">
+      <div className="dice-container">
+        <Dice
+          side={side}
+          shaking={shaking}
+          color={color}
+        />
+      </div>
+    </div>
+  );
 }
